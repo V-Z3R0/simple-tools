3289d4dcf67df53a5f9a1697443cfe564c1d4fd7
V-Z3R0 <31276820+V-Z3R0@users.noreply.github.com>
2020-10-28 08:11:30 +0700

    Update http-remover.py

--- http-remover.py
+++ http-remover.py
@@ -1,7 +1,7 @@
 x = input("input list : ")
 y = input("save as : ")
-with open((x), "r") as list:
- with open((y), "w") as ress:
+with open(x, "r") as list:
+ with open(y, "w") as ress:
   for line in list:
    if "http://" in line:
     ress.write(line.replace('http://',''))
